17f34847173b064686dc287193c79036b1d18528
Aleksandr D <none>
2024-04-20 02:21:37 +0700

    renamed variable filename to file_path, added constants that holds book name, book folder name separately, added some simple text cleaning added to epub_get_book_txt() possibility to send list of files

--- epub_reader.py
+++ epub_reader.py
@@ -9,7 +9,10 @@ class BookEpub:
         # Check the filename for existence.
         if not os.path.exists(filename):
             raise FileNotFoundError(f"File {filename} not found")
-        self.filename = filename
+        self.file_path = filename
+        self.folder_path = os.path.dirname(self.file_path)
+        self.file_name_ext = os.path.basename(self.file_path)
+        self.file_name_no_ext = os.path.splitext(self.file_name_ext)[0]
         self.extracted_path = None
         self.book_parts = []
     
@@ -19,7 +22,7 @@ class BookEpub:
         """
         Prints the list of files it the EPUB archive.
         """
-        with zipfile.ZipFile(self.filename, 'r') as zf:
+        with zipfile.ZipFile(self.file_path, 'r') as zf:
             for f in zf.namelist():
                 print(f)
 
@@ -30,7 +33,7 @@ class BookEpub:
         Returns usirted list of files it the EPUB archive.
         """
         content = list()
-        with zipfile.ZipFile(self.filename, 'r') as zf:
+        with zipfile.ZipFile(self.file_path, 'r') as zf:
             for f in zf.namelist():
                 content.append(f)
         return content
@@ -42,7 +45,7 @@ class BookEpub:
         Returns sorted list of .xhtml files it the EPUB archive.
         """
         content = []
-        with zipfile.ZipFile(self.filename, 'r') as zf:
+        with zipfile.ZipFile(self.file_path, 'r') as zf:
             for f in zf.namelist():
                 if "xhtml" in f:
                     content.append(f)
@@ -57,10 +60,10 @@ class BookEpub:
         b_pwd - password if any and if applicapabel at all.
         """
         if b_path != None:
-            b_path = os.path.dirname(self.filename)
-        b_name = os.path.splitext(os.path.basename(self.filename))[0]
+            b_path = os.path.dirname(self.file_path)
+        b_name = os.path.splitext(os.path.basename(self.file_path))[0]
         self.extracted_path = b_path + '/' + b_name
-        with zipfile.ZipFile(self.filename, 'r') as zf:
+        with zipfile.ZipFile(self.file_path, 'r') as zf:
             for f in zf.namelist():
                 zf.extract(f, self.extracted_path, b_pwd)
 
@@ -69,11 +72,12 @@ class BookEpub:
         """
         Gets list of book part names.
         """
-        with zipfile.ZipFile(self.filename, 'r') as zf:
+        with zipfile.ZipFile(self.file_path, 'r') as zf:
             for f in zf.namelist():
                 if ".xhtml" in str(f):
                     self.book_parts.append(str(f))
         self.book_parts = sorted(self.book_parts)
+        return self.book_parts
 
 
     def epub_get_text_from_parts(self, part_name=None):
@@ -87,7 +91,10 @@ class BookEpub:
             raise FileNotFoundError(f"File {part_name} not found")
         with open(part_name, "r", encoding="utf-8") as f:
             text = BeautifulSoup(f.read(), "html.parser").get_text(separator=" ", strip=True)
-        text = re.sub("\[\d+\]", "", text)  # clean text from links like [123]
+        text = re.sub(r"\[\d+\]", "", text)  # clean text from links like [123]
+        text = re.sub(r"\{\d+\}", "", text)  # clean text from links like {123}
+        text = text.replace(" . ", ". ")    # with such simple replacement it works faster then with re.sub
+        text = text.replace(" , ", ", ")    # with such simple replacement it works faster then with re.sub
         return(text)
     
 
@@ -112,11 +119,11 @@ class BookEpub:
         # text = text.replace(" \n, ", ", ")  # for nice printout of text
         # text = text.replace("\n, ", ",")    # for nice printout of text
             
-        text = re.sub("\[\d+\]", "", text)  # clean text from links like [123]
+        text = re.sub(r"\[\d+\]", "", text)  # clean text from links like [123]
         print(text)
     
 
-    def epub_get_book_txt(self, book_extracted_path=None):
+    def epub_get_book_txt(self, book_extracted_path=None, book_parts_list=None):
         if self.extracted_path != None and book_extracted_path == None:
             book_extracted_path == self.extracted_path
         elif self.extracted_path == None and book_extracted_path == None:
@@ -125,9 +132,15 @@ class BookEpub:
         if not os.path.exists(book_extracted_path):
             raise FileNotFoundError(f"Folder {book_extracted_path} not found")
         book_text = list()
-        part_names = self.epub_get_xhtml_names()
-        for name in part_names:
-            book_text.append(self.epub_get_text_from_parts(book_extracted_path + "/" + name))
+        if book_parts_list == None:
+            book_parts_list = self.epub_get_xhtml_names()
+        
+        for name in book_parts_list:
+            # Check the part name for existence.
+            book_part_path = book_extracted_path + "/" + name
+            if not os.path.exists(book_part_path):
+                raise FileNotFoundError(f"File {book_part_path} not found")
+            book_text.append(self.epub_get_text_from_parts(book_part_path))
         return " ".join(book_text)
 
 
