e0a011f7e64ddbea24ed0d6f778fd59ad11e3f37
Aleksandr D <none>
2024-04-20 00:47:01 +0700

    add epub_reader.py with BookEpub class that helps to unpack .epub book and extract text from the unpacked .xhtml files

---
+++ epub_reader.py
@@ -0,0 +1,143 @@
+import zipfile
+import os
+from bs4 import BeautifulSoup
+import re
+
+
+class BookEpub:
+    def __init__(self, filename):
+        # Check the filename for existence.
+        if not os.path.exists(filename):
+            raise FileNotFoundError(f"File {filename} not found")
+        self.filename = filename
+        self.extracted_path = None
+        self.book_parts = []
+    
+
+    # Done
+    def epub_print_content(self):
+        """
+        Prints the list of files it the EPUB archive.
+        """
+        with zipfile.ZipFile(self.filename, 'r') as zf:
+            for f in zf.namelist():
+                print(f)
+
+
+    # Done
+    def epub_get_content(self):
+        """
+        Returns usirted list of files it the EPUB archive.
+        """
+        content = list()
+        with zipfile.ZipFile(self.filename, 'r') as zf:
+            for f in zf.namelist():
+                content.append(f)
+        return content
+
+
+    # Done
+    def epub_get_xhtml_names(self):
+        """
+        Returns sorted list of .xhtml files it the EPUB archive.
+        """
+        content = []
+        with zipfile.ZipFile(self.filename, 'r') as zf:
+            for f in zf.namelist():
+                if "xhtml" in f:
+                    content.append(f)
+        return sorted(content)
+
+
+    # Done
+    def epub_unpack(self, b_path=None, b_pwd=None):
+        """
+        Unpacks the EPUB file into folder named as EPUB file located in parent folder "books" by default.
+        b_path - folder to where extract book. by default its the same place where book is located and it assumed to be "books" folder.
+        b_pwd - password if any and if applicapabel at all.
+        """
+        if b_path != None:
+            b_path = os.path.dirname(self.filename)
+        b_name = os.path.splitext(os.path.basename(self.filename))[0]
+        self.extracted_path = b_path + '/' + b_name
+        with zipfile.ZipFile(self.filename, 'r') as zf:
+            for f in zf.namelist():
+                zf.extract(f, self.extracted_path, b_pwd)
+
+    
+    def epub_get_part_names(self):
+        """
+        Gets list of book part names.
+        """
+        with zipfile.ZipFile(self.filename, 'r') as zf:
+            for f in zf.namelist():
+                if ".xhtml" in str(f):
+                    self.book_parts.append(str(f))
+        self.book_parts = sorted(self.book_parts)
+
+
+    def epub_get_text_from_parts(self, part_name=None):
+        """
+        Opens part of extracted epud, extract text, clean it from html code and return cleanned text
+        """
+        if part_name == None:
+            part_name = input("Enter full path to the file you need:\n")
+        # Check the filename for existence.
+        if not os.path.exists(part_name):
+            raise FileNotFoundError(f"File {part_name} not found")
+        with open(part_name, "r", encoding="utf-8") as f:
+            text = BeautifulSoup(f.read(), "html.parser").get_text(separator=" ", strip=True)
+        text = re.sub("\[\d+\]", "", text)  # clean text from links like [123]
+        return(text)
+    
+
+    def epub_print_text_from_parts(self, part_name=None):
+        """
+        Opens part of extracted epud, extract text, clean it from html code and print cleanned text
+        """
+        if part_name == None:
+            part_name = input("Enter full path to the file you need:\n")
+        # Check the filename for existence.
+        if not os.path.exists(part_name):
+            raise FileNotFoundError(f"File {part_name} not found")
+        with open(part_name, 'r', encoding='utf-8') as f:
+            # for text that will be splitted later for one sentence per row:
+            text = BeautifulSoup(f.read(), "html.parser").get_text(separator=" ", strip=True)
+
+            # for text expected to be printed out later as paragraphs
+            # text = BeautifulSoup(f.read(), "html.parser").get_text(separator="\n", strip=True)
+        # text = text.replace(" . ", ". ")    # for nice printout of text
+        # text = text.replace(" \n. ", ". ")  # for nice printout of text
+        # text = text.replace("\n. ", ". ")   # for nice printout of text
+        # text = text.replace(" \n, ", ", ")  # for nice printout of text
+        # text = text.replace("\n, ", ",")    # for nice printout of text
+            
+        text = re.sub("\[\d+\]", "", text)  # clean text from links like [123]
+        print(text)
+    
+
+    def epub_get_book_txt(self, book_extracted_path=None):
+        if self.extracted_path != None and book_extracted_path == None:
+            book_extracted_path == self.extracted_path
+        elif self.extracted_path == None and book_extracted_path == None:
+            book_extracted_path = input("Enter folder path for the place where the book is located:\n")
+        # Check the folder path for existence.
+        if not os.path.exists(book_extracted_path):
+            raise FileNotFoundError(f"Folder {book_extracted_path} not found")
+        book_text = list()
+        part_names = self.epub_get_xhtml_names()
+        for name in part_names:
+            book_text.append(self.epub_get_text_from_parts(book_extracted_path + "/" + name))
+        return " ".join(book_text)
+
+
+# if __name__ == "__main__":
+#     file_book = 'books/test.epub'
+#     b = BookEpub(file_book)
+#     # b.epub_print_content()
+#     # b.epub_unpack(file_book)
+#     # b.epub_get_part_names()
+#     # b.epub_get_text_from_parts("books/test/index_split_001.xhtml")
+#     # b.epub_print_text_from_parts("books/test/index_split_002.xhtml")
+#     book = b.epub_get_book_txt("books/test")
+#     # print(book[:5000])
